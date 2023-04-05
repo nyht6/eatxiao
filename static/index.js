f91a32a664fed2907189e59f95240f9e58798a11
nyht6 <129947179+nyht6@users.noreply.github.com>
2023-04-05 17:50:49 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -14,7 +14,7 @@ let __Time = 20;
 let __k = 4;
 let _close = false;
 let _fsj = false;
-var url = 'https://nyht6.github.io/eatxiao/static/images/ClickBefore.PNG';
+var url = 'https://nyht6.github.io/eatxiao/static/image/ClickBefore.PNG';
 
 function isplaying() {
     return document.getElementById('welcome').style.display == 'none' &&
